8d4661b29e5abc61361e1f58be57651dd187fc1b
Nek <trunks_17_54@hotmail.com>
2023-08-25 20:26:27 -0600

    weatherv3.1

--- js/constants.js
+++ js/constants.js
@@ -1,11 +1,28 @@
-export const API_KEY = '88af7171729df8cce0e591ed2b0388ed'
-export const BASE_API = 'https://api.openweathermap.org/data/2.5/'
-
+export const API_KEY = "88af7171729df8cce0e591ed2b0388ed";
+export const BASE_API = "https://api.openweathermap.org/data/2.5/";
+const city = "Tampico";
+const humidityAja = document.getElementById("current-weather-humidity");
 export const weatherConditionsCodes = {
-    2: 'rainy',
-    3: 'cloudy',
-    5: 'rainy',
-    6: 'drizzle',
-    8: 'cloudy',
-    7: 'clean',
-}
+  2: "rainy",
+  3: "cloudy",
+  5: "rainy",
+  6: "drizzle",
+  8: "cloudy",
+  7: "clean",
+};
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.main && data.main.humidity !== undefined) {
+      const humidity = data.main.humidity;
+      humidityAja.textContent = `${humidity}%`;
+    } else {
+      console.log("No se pudo obtener la información de humedad.");
+    }
+  })
+  .catch((error) => {
+    console.error("Ha ocurrido un error:", error);
+  });
--- js/current-weather.js
+++ js/current-weather.js
@@ -1,95 +1,98 @@
-import weather from '../data/current-weather.js'
-import { formatDate, formatTemp } from './utils/format-data.js'
-import { weatherConditionsCodes } from './constants.js'
-import { getLatLon } from './geolocation.js'
-import { getCurrentWeather } from './services/weather.js'
+import weather from "../data/current-weather.js";
+import { formatDate, formatTemp } from "./utils/format-data.js";
+import { weatherConditionsCodes } from "./constants.js";
+import { getLatLon } from "./geolocation.js";
+import { getCurrentWeather } from "./services/weather.js";
 // weather.name
-weatherConditionsCodes
-    String(weather.weather[0].id).charAt(0)
+weatherConditionsCodes;
+String(weather.weather[0].id).charAt(0);
 
 function setCurrentCity($el, city) {
-    $el.textContent = city
+  $el.textContent = city;
 }
 
-
 function setCurrentDate($el) {
-    const date = new Date()
-    const formattedDate = formatDate(date)
-    $el.textContent = formattedDate
+  const date = new Date();
+  const formattedDate = formatDate(date);
+  $el.textContent = formattedDate;
 }
 
 function setCurrentTemp($el, temp) {
-    $el.textContent = formatTemp(temp)
+  $el.textContent = formatTemp(temp);
 }
 
 function solarStatus(sunriseTime, sunsetTime) {
-    const currentHours = new Date().getHours()
-    const sunriseHours = sunriseTime.getHours()
-    const sunsetHours = sunsetTime.getHours()
+  const currentHours = new Date().getHours();
+  const sunriseHours = sunriseTime.getHours();
+  const sunsetHours = sunsetTime.getHours();
 
-    if (currentHours > sunsetHours || currentHours < sunriseHours) {
-        return 'night'
-    }
-    return 'morning'
+  if (currentHours > sunsetHours || currentHours < sunriseHours) {
+    return "night";
+  }
+  return "morning";
 }
 
 function setBackground($el, conditionCode, solarStatus) {
-    const weatherType = weatherConditionsCodes[conditionCode]
-    const size = window.matchMedia('(-webkit-min-device-pixel-ratio: 2)').matches ? '@2x' : ''
-    // let size = ''
-    // if window.matchMedia('(-webkit-min-device-pixel-ratio: 2)') {
-    //     size = '@2x'
-    // }
-    // true ? '@2x' : ''
-    $el.style.backgroundImage = `url(./images/${solarStatus}-${weatherType}${size}.jpg)`
+  const weatherType = weatherConditionsCodes[conditionCode];
+  const size = window.matchMedia("(-webkit-min-device-pixel-ratio: 2)").matches
+    ? "@2x"
+    : "";
+  // let size = ''
+  // if window.matchMedia('(-webkit-min-device-pixel-ratio: 2)') {
+  //     size = '@2x'
+  // }
+  // true ? '@2x' : ''
+  $el.style.backgroundImage = `url(./images/${solarStatus}-${weatherType}${size}.jpg)`;
 }
 
 function showCurrentWeather($app, $loader) {
-    $app.hidden = false
-    $loader.hidden = true
-} 
+  $app.hidden = false;
+  $loader.hidden = true;
+}
 
 function configCurrentWeather(weather) {
-    const $app = document.querySelector('#app')
-    const $loading = document.querySelector('#loading')
+  const $app = document.querySelector("#app");
+  const $loading = document.querySelector("#loading");
 
-    // loader
-    showCurrentWeather($app, $loading)
-    // date
-    const $currentWeatherDate = document.querySelector('#current-weather-date')
-    setCurrentDate($currentWeatherDate)
-    // city
-    const $currentWeatherCity = document.querySelector('#current-weather-city')
-    const city = weather.name
-    setCurrentCity($currentWeatherCity, city)
-    // temp
-    const $currentWeatherTemp = document.querySelector('#current-weather-temp')
-    const temp = weather.main.temp
-    setCurrentTemp($currentWeatherTemp, temp)
+  // loader
+  showCurrentWeather($app, $loading);
+  // date
+  const $currentWeatherDate = document.querySelector("#current-weather-date");
+  setCurrentDate($currentWeatherDate);
+  // city
+  const $currentWeatherCity = document.querySelector("#current-weather-city");
+  const city = weather.name;
+  setCurrentCity($currentWeatherCity, city);
+  // temp
+  const $currentWeatherTemp = document.querySelector("#current-weather-temp");
+  const temp = weather.main.temp;
+  setCurrentTemp($currentWeatherTemp, temp);
+  // humidity
 
-    // background
-    const sunriseTime = new Date (weather.sys.sunrise * 1000)
-    const sunsetTime = new Date (weather.sys.sunset * 1000)
-    const conditionCode = String(weather.weather[0].id).charAt(0)
-    setBackground($app, conditionCode, solarStatus(sunriseTime, sunsetTime))
-
+  // background
+  const sunriseTime = new Date(weather.sys.sunrise * 1000);
+  const sunsetTime = new Date(weather.sys.sunset * 1000);
+  const conditionCode = String(weather.weather[0].id).charAt(0);
+  setBackground($app, conditionCode, solarStatus(sunriseTime, sunsetTime));
 }
 
 export default async function currentWeather() {
-    // GEO // API // weather // Config
-    const { lat, lon, isError } = await getLatLon()
-    if (isError) return console.log('Fallo al ubicarte, enciende tu GPS')
-    // console.log(lat,lon)
-    // const latlon = getCurrentPosition()
-    // getCurrentPosition()
-    // .then((data) => {
-    //     console.log('siwe', data)
-    // })
-    // .catch((message) => {
-    //     console.log(message)
-    // })
-    const { isError: currentWeatherError, data: weather } = await getCurrentWeather(lat, lon)
-    if (currentWeatherError) return console.log('oh! ha ocurrido un error trayendo los datos del clima')
-    configCurrentWeather(weather)
-    // console.log(weather)
+  // GEO // API // weather // Config
+  const { lat, lon, isError } = await getLatLon();
+  if (isError) return console.log("Fallo al ubicarte, enciende tu GPS");
+  // console.log(lat,lon)
+  // const latlon = getCurrentPosition()
+  // getCurrentPosition()
+  // .then((data) => {
+  //     console.log('siwe', data)
+  // })
+  // .catch((message) => {
+  //     console.log(message)
+  // })
+  const { isError: currentWeatherError, data: weather } =
+    await getCurrentWeather(lat, lon);
+  if (currentWeatherError)
+    return console.log("oh! ha ocurrido un error trayendo los datos del clima");
+  configCurrentWeather(weather);
+  // console.log(weather)
 }
